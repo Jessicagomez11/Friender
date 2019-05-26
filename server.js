fad29d26e79ee8d72f2f23e05084718a4170f6cd
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-05-25 20:57:21 -0700

    it's posting! needs logic

--- server.js
+++ server.js
@@ -44,13 +44,9 @@ app.get("/api/friends", function (req, res) {
 // require("./app/routing/htmlRoutes")(app);
 
 app.post("/api/friends", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
+    
     var newFriend = req.body;
 
-    // Using a RegEx Pattern to remove spaces from newFriend
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
 
     console.log(newFriend);
 
